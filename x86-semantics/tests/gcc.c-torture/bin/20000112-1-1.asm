9d917d385b40c7c8f9aefab6024b20f69ebbc68e
Sandeep Dasgupta <sdasgup3@illinois.edu>
2018-06-26 20:09:39 -0500

    https://github.com/sdasgup3/binary-decompilation/issues/77: wip

--- x86-semantics/tests/gcc.c-torture/bin/20000112-1-1.asm
+++ x86-semantics/tests/gcc.c-torture/bin/20000112-1-1.asm
@@ -1,14 +1,20 @@
+	.file	"20000112-1.c"
+	.text
+	.globl	strchr
+	.type	strchr, @function
 strchr:
 	movzbl	(%rdi), %edx
 	cmpb	%sil, %dl
-	je	L2
+	je	.L2
 	movl	$0, %eax
 	addq	$1, %rdi
 	testb	%dl, %dl
 	cmovne	%rdi, %rax
 	ret
-L2:
+.L2:
 	ret
+	.size	strchr, .-strchr
+	.type	special_format, @function
 special_format:
 	pushq	%rbx
 	movq	%rdi, %rbx
@@ -16,35 +22,49 @@ special_format:
 	call	strchr
 	movl	$1, %edx
 	testq	%rax, %rax
-	jne	L6
+	jne	.L6
 	movl	$86, %esi
 	movq	%rbx, %rdi
 	call	strchr
 	movl	$1, %edx
 	testq	%rax, %rax
-	jne	L6
+	jne	.L6
 	movl	$83, %esi
 	movq	%rbx, %rdi
 	call	strchr
 	movl	$1, %edx
 	testq	%rax, %rax
-	jne	L6
+	jne	.L6
 	movl	$110, %esi
 	movq	%rbx, %rdi
 	call	strchr
 	testq	%rax, %rax
 	setne	%dl
 	movzbl	%dl, %edx
-L6:
+.L6:
 	movl	%edx, %eax
 	popq	%rbx
 	ret
+	.size	special_format, .-special_format
+	.globl	exit
+	.type	exit, @function
 exit:
 	ret
+	.size	exit, .-exit
+	.globl	abort
+	.type	abort, @function
 abort:
 	ret
-LC0:
+	.size	abort, .-abort
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.LC0:
 	.string	"ee"
-LC1:
+.LC1:
 	.string	"*e"
+	.text
+	.globl	main
+	.type	main, @function
 main:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 4.9.4-2ubuntu1) 4.9.4"
+	.section	.note.GNU-stack,"",@progbits
--- x86-semantics/tests/gcc.c-torture/bin/20000112-1-4.asm
+++ x86-semantics/tests/gcc.c-torture/bin/20000112-1-4.asm
@@ -1,22 +1,31 @@
-Ltext0:
+	.file	"20000112-1.c"
+	.text
+.Ltext0:
 	.cfi_sections	.debug_frame
-LCOLDB0:
-LHOTB0:
+	.section	.text.unlikely,"ax",@progbits
+.LCOLDB0:
+	.text
+.LHOTB0:
 	.p2align 4,,15
-Ltext_cold0:
+	.section	.text.unlikely
+.Ltext_cold0:
+	.text
+	.globl	strchr
+	.type	strchr, @function
 strchr:
-LFB0:
+.LFB0:
+	.file 1 "src/library/string.h"
 	.loc 1 1 0
 	.cfi_startproc
-LVL0:
+.LVL0:
 	.loc 1 2 0
 	movzbl	(%rdi), %edx
 	cmpb	%sil, %dl
-	je	L2
-LVL1:
+	je	.L2
+.LVL1:
 	.loc 1 5 0
 	addq	$1, %rdi
-LVL2:
+.LVL2:
 	.loc 1 4 0
 	xorl	%eax, %eax
 	.loc 1 5 0
@@ -25,66 +34,95 @@ LVL2:
 	ret
 	.p2align 4,,10
 	.p2align 3
-L2:
+.L2:
 	.loc 1 7 0
 	ret
 	.cfi_endproc
-LFE0:
-LCOLDE0:
-LHOTE0:
-LCOLDB1:
-LHOTB1:
+.LFE0:
+	.size	strchr, .-strchr
+	.section	.text.unlikely
+.LCOLDE0:
+	.text
+.LHOTE0:
+	.section	.text.unlikely
+.LCOLDB1:
+	.text
+.LHOTB1:
 	.p2align 4,,15
+	.globl	exit
+	.type	exit, @function
 exit:
-LFB1:
+.LFB1:
+	.file 2 "src/library/stdlib.h"
 	.loc 2 1 0
 	.cfi_startproc
-LVL3:
+.LVL3:
 	ret
 	.cfi_endproc
-LFE1:
-LCOLDE1:
-LHOTE1:
-LCOLDB2:
-LHOTB2:
+.LFE1:
+	.size	exit, .-exit
+	.section	.text.unlikely
+.LCOLDE1:
+	.text
+.LHOTE1:
+	.section	.text.unlikely
+.LCOLDB2:
+	.text
+.LHOTB2:
 	.p2align 4,,15
+	.globl	abort
+	.type	abort, @function
 abort:
-LFB2:
+.LFB2:
 	.loc 2 5 0
 	.cfi_startproc
 	ret
 	.cfi_endproc
-LFE2:
-LCOLDE2:
-LHOTE2:
-LCOLDB3:
-LHOTB3:
+.LFE2:
+	.size	abort, .-abort
+	.section	.text.unlikely
+.LCOLDE2:
+	.text
+.LHOTE2:
+	.section	.text.unlikely
+.LCOLDB3:
+	.section	.text.startup,"ax",@progbits
+.LHOTB3:
 	.p2align 4,,15
+	.globl	main
+	.type	main, @function
 main:
-LFB4:
+.LFB4:
+	.file 3 "./src/20000112-1.c"
 	.loc 3 15 0
 	.cfi_startproc
 	.cfi_endproc
-LFE4:
-LCOLDE3:
-LHOTE3:
-Letext0:
-Letext_cold0:
-Ldebug_info0:
+.LFE4:
+	.size	main, .-main
+	.section	.text.unlikely
+.LCOLDE3:
+	.section	.text.startup
+.LHOTE3:
+	.text
+.Letext0:
+	.section	.text.unlikely
+.Letext_cold0:
+	.section	.debug_info,"",@progbits
+.Ldebug_info0:
 	.long	0x10b
 	.value	0x4
-	.long	Ldebug_abbrev0
+	.long	.Ldebug_abbrev0
 	.byte	0x8
 	.uleb128 0x1
-	.long	LASF2
+	.long	.LASF2
 	.byte	0x1
-	.long	LASF3
-	.long	LASF4
-	.long	Ldebug_ranges0+0
+	.long	.LASF3
+	.long	.LASF4
+	.long	.Ldebug_ranges0+0
 	.quad	0
-	.long	Ldebug_line0
+	.long	.Ldebug_line0
 	.uleb128 0x2
-	.long	LASF5
+	.long	.LASF5
 	.byte	0x1
 	.byte	0x1
 	.long	0x4c
@@ -107,7 +145,7 @@ Ldebug_info0:
 	.uleb128 0x5
 	.byte	0x1
 	.byte	0x6
-	.long	LASF0
+	.long	.LASF0
 	.uleb128 0x4
 	.byte	0x8
 	.long	0x5f
@@ -118,32 +156,32 @@ Ldebug_info0:
 	.byte	0x5
 	.string	"int"
 	.uleb128 0x8
-	.long	LASF6
+	.long	.LASF6
 	.byte	0x2
 	.byte	0x5
 	.byte	0x1
 	.uleb128 0x9
-	.long	LASF7
+	.long	.LASF7
 	.byte	0x2
 	.byte	0x1
 	.byte	0x1
 	.long	0x8b
 	.uleb128 0xa
-	.long	LASF1
+	.long	.LASF1
 	.byte	0x2
 	.byte	0x1
 	.long	0x64
 	.byte	0
 	.uleb128 0xb
 	.long	0x29
-	.quad	LFB0
-	.quad	.LFE0-LFB0
+	.quad	.LFB0
+	.quad	.LFE0-.LFB0
 	.uleb128 0x1
 	.byte	0x9c
 	.long	0xb7
 	.uleb128 0xc
 	.long	0x39
-	.long	LLST0
+	.long	.LLST0
 	.uleb128 0xd
 	.long	0x42
 	.uleb128 0x1
@@ -151,8 +189,8 @@ Ldebug_info0:
 	.byte	0
 	.uleb128 0xb
 	.long	0x73
-	.quad	LFB1
-	.quad	.LFE1-LFB1
+	.quad	.LFB1
+	.quad	.LFE1-.LFB1
 	.uleb128 0x1
 	.byte	0x9c
 	.long	0xda
@@ -163,21 +201,22 @@ Ldebug_info0:
 	.byte	0
 	.uleb128 0xe
 	.long	0x6b
-	.quad	LFB2
-	.quad	.LFE2-LFB2
+	.quad	.LFB2
+	.quad	.LFE2-.LFB2
 	.uleb128 0x1
 	.byte	0x9c
 	.uleb128 0xf
-	.long	LASF8
+	.long	.LASF8
 	.byte	0x3
 	.byte	0xe
 	.long	0x64
-	.quad	LFB4
-	.quad	.LFE4-LFB4
+	.quad	.LFB4
+	.quad	.LFE4-.LFB4
 	.uleb128 0x1
 	.byte	0x9c
 	.byte	0
-Ldebug_abbrev0:
+	.section	.debug_abbrev,"",@progbits
+.Ldebug_abbrev0:
 	.uleb128 0x1
 	.uleb128 0x11
 	.byte	0x1
@@ -392,60 +431,67 @@ Ldebug_abbrev0:
 	.byte	0
 	.byte	0
 	.byte	0
-Ldebug_loc0:
-LLST0:
-	.quad	LVL0
-	.quad	LVL1
+	.section	.debug_loc,"",@progbits
+.Ldebug_loc0:
+.LLST0:
+	.quad	.LVL0
+	.quad	.LVL1
 	.value	0x1
 	.byte	0x55
-	.quad	LVL1
-	.quad	LVL2
+	.quad	.LVL1
+	.quad	.LVL2
 	.value	0x3
 	.byte	0x75
 	.sleb128 1
 	.byte	0x9f
-	.quad	LVL2
-	.quad	LFE0
+	.quad	.LVL2
+	.quad	.LFE0
 	.value	0x1
 	.byte	0x55
 	.quad	0
 	.quad	0
+	.section	.debug_aranges,"",@progbits
 	.long	0x3c
 	.value	0x2
-	.long	Ldebug_info0
+	.long	.Ldebug_info0
 	.byte	0x8
 	.byte	0
 	.value	0
 	.value	0
-	.quad	Ltext0
-	.quad	.Letext0-Ltext0
-	.quad	LFB4
-	.quad	.LFE4-LFB4
+	.quad	.Ltext0
+	.quad	.Letext0-.Ltext0
+	.quad	.LFB4
+	.quad	.LFE4-.LFB4
 	.quad	0
 	.quad	0
-Ldebug_ranges0:
-	.quad	Ltext0
-	.quad	Letext0
-	.quad	LFB4
-	.quad	LFE4
+	.section	.debug_ranges,"",@progbits
+.Ldebug_ranges0:
+	.quad	.Ltext0
+	.quad	.Letext0
+	.quad	.LFB4
+	.quad	.LFE4
 	.quad	0
 	.quad	0
-Ldebug_line0:
-LASF8:
+	.section	.debug_line,"",@progbits
+.Ldebug_line0:
+	.section	.debug_str,"MS",@progbits,1
+.LASF8:
 	.string	"main"
-LASF7:
+.LASF7:
 	.string	"exit"
-LASF4:
+.LASF4:
 	.string	"/home/sdasgup3/x86-semantics/tests/gcc.c-torture"
-LASF5:
+.LASF5:
 	.string	"strchr"
-LASF1:
+.LASF1:
 	.string	"code"
-LASF3:
+.LASF3:
 	.string	"./src/20000112-1.c"
-LASF6:
+.LASF6:
 	.string	"abort"
-LASF0:
+.LASF0:
 	.string	"char"
-LASF2:
+.LASF2:
 	.string	"GNU C 4.9.4 -march=haswell -g -O3 -fno-asynchronous-unwind-tables -fstack-protector-strong"
+	.ident	"GCC: (Ubuntu 4.9.4-2ubuntu1) 4.9.4"
+	.section	.note.GNU-stack,"",@progbits
