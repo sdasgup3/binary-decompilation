eb49571b329e590f088e14430ba79f4e934b0376
Sandeep Dasgupta <sdasgup3@illinois.edu>
2018-06-26 20:04:46 -0500

    https://github.com/sdasgup3/binary-decompilation/issues/77: wip

--- x86-semantics/tests/gcc.c-torture/bin/20000112-1-2.asm
+++ x86-semantics/tests/gcc.c-torture/bin/20000112-1-2.asm
@@ -1,15 +1,10 @@
-	.file	"20000112-1.c"
-	.section	.text.unlikely,"ax",@progbits
-.LCOLDB0:
-	.text
-.LHOTB0:
+LCOLDB0:
+LHOTB0:
 	.p2align 4,,15
-	.globl	strchr
-	.type	strchr, @function
 strchr:
 	movzbl	(%rdi), %edx
 	cmpb	%sil, %dl
-	je	.L2
+	je	L2
 	addq	$1, %rdi
 	xorl	%eax, %eax
 	testb	%dl, %dl
@@ -17,53 +12,27 @@ strchr:
 	ret
 	.p2align 4,,10
 	.p2align 3
-.L2:
+L2:
 	ret
-	.size	strchr, .-strchr
-	.section	.text.unlikely
-.LCOLDE0:
-	.text
-.LHOTE0:
-	.section	.text.unlikely
-.LCOLDB1:
-	.text
-.LHOTB1:
+LCOLDE0:
+LHOTE0:
+LCOLDB1:
+LHOTB1:
 	.p2align 4,,15
-	.globl	exit
-	.type	exit, @function
 exit:
 	ret
-	.size	exit, .-exit
-	.section	.text.unlikely
-.LCOLDE1:
-	.text
-.LHOTE1:
-	.section	.text.unlikely
-.LCOLDB2:
-	.text
-.LHOTB2:
+LCOLDE1:
+LHOTE1:
+LCOLDB2:
+LHOTB2:
 	.p2align 4,,15
-	.globl	abort
-	.type	abort, @function
 abort:
 	ret
-	.size	abort, .-abort
-	.section	.text.unlikely
-.LCOLDE2:
-	.text
-.LHOTE2:
-	.section	.text.unlikely
-.LCOLDB3:
-	.section	.text.startup,"ax",@progbits
-.LHOTB3:
+LCOLDE2:
+LHOTE2:
+LCOLDB3:
+LHOTB3:
 	.p2align 4,,15
-	.globl	main
-	.type	main, @function
 main:
-	.size	main, .-main
-	.section	.text.unlikely
-.LCOLDE3:
-	.section	.text.startup
-.LHOTE3:
-	.ident	"GCC: (Ubuntu 4.9.4-2ubuntu1) 4.9.4"
-	.section	.note.GNU-stack,"",@progbits
+LCOLDE3:
+LHOTE3:
