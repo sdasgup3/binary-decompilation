d714cc7115d880378b00db38fef0b1bbbf434a39
Sandeep Dasgupta <sdasgup3@illinois.edu>
2018-06-26 20:09:39 -0500

    https://github.com/sdasgup3/binary-decompilation/issues/77: wip

--- x86-semantics/tests/gcc.c-torture/bin/20000112-1-5.asm
+++ x86-semantics/tests/gcc.c-torture/bin/20000112-1-5.asm
@@ -1,32 +1,63 @@
-LCOLDB0:
-LHOTB0:
+	.file	"20000112-1.c"
+	.section	.text.unlikely,"ax",@progbits
+.LCOLDB0:
+	.text
+.LHOTB0:
+	.globl	strchr
+	.type	strchr, @function
 strchr:
 	movb	(%rdi), %dl
 	cmpb	%sil, %dl
-	je	L2
+	je	.L2
 	leaq	1(%rdi), %rax
 	testb	%dl, %dl
 	movl	$0, %edx
 	cmove	%rdx, %rax
 	ret
-L2:
+.L2:
 	ret
-LCOLDE0:
-LHOTE0:
-LCOLDB1:
-LHOTB1:
+	.size	strchr, .-strchr
+	.section	.text.unlikely
+.LCOLDE0:
+	.text
+.LHOTE0:
+	.section	.text.unlikely
+.LCOLDB1:
+	.text
+.LHOTB1:
+	.globl	exit
+	.type	exit, @function
 exit:
 	ret
-LCOLDE1:
-LHOTE1:
-LCOLDB2:
-LHOTB2:
+	.size	exit, .-exit
+	.section	.text.unlikely
+.LCOLDE1:
+	.text
+.LHOTE1:
+	.section	.text.unlikely
+.LCOLDB2:
+	.text
+.LHOTB2:
+	.globl	abort
+	.type	abort, @function
 abort:
 	ret
-LCOLDE2:
-LHOTE2:
-LCOLDB3:
-LHOTB3:
+	.size	abort, .-abort
+	.section	.text.unlikely
+.LCOLDE2:
+	.text
+.LHOTE2:
+	.section	.text.unlikely
+.LCOLDB3:
+	.section	.text.startup,"ax",@progbits
+.LHOTB3:
+	.globl	main
+	.type	main, @function
 main:
-LCOLDE3:
-LHOTE3:
+	.size	main, .-main
+	.section	.text.unlikely
+.LCOLDE3:
+	.section	.text.startup
+.LHOTE3:
+	.ident	"GCC: (Ubuntu 4.9.4-2ubuntu1) 4.9.4"
+	.section	.note.GNU-stack,"",@progbits
