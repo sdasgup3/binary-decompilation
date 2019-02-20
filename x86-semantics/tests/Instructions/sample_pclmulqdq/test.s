7a94d53991970e1cd025f44bcc9ba3dee53ad529
Sandeep Dasgupta <sdasgup3@illinois.edu>
2019-02-20 10:10:52 -0600

    sample single instruction testing

--- x86-semantics/tests/Instructions/sample_pclmulqdq/test.s
+++ x86-semantics/tests/Instructions/sample_pclmulqdq/test.s
@@ -4,32 +4,47 @@
 _start:
   // PCLMULQDQ  
   movq  $0,  %rax
+  movq  $0xFEFEFEFEFEFEFEFE,  %rbx
   movq  %rax, %xmm0
-  movddup %xmm0, %xmm1
+  movq  %rbx, %xmm2
+  movddup %xmm0, %xmm0
+  movddup %xmm2, %xmm2
   
   pclmulqdq $0, %xmm1, %xmm2
   
   movq  $0x80,  %rax
+  movq  $0xF7F7F7F7,  %rbx
   movq  %rax, %xmm0
-  movddup %xmm0, %xmm1
+  movq  %rbx, %xmm2
+  movddup %xmm0, %xmm0
+  movddup %xmm2, %xmm2
   
   pclmulqdq $127, %xmm1, %xmm2
   
   movq  $0x0F0F,  %rax
+  movq  $0x0F0F,  %rbx
   movq  %rax, %xmm0
-  movddup %xmm0, %xmm1
+  movq  %rbx, %xmm2
+  movddup %xmm0, %xmm0
+  movddup %xmm2, %xmm2
   
   pclmulqdq $11, %xmm1, %xmm2
   
   movq  $0xF7F7F7F7,  %rax
+  movq  $0x80,  %rbx
   movq  %rax, %xmm0
-  movddup %xmm0, %xmm1
+  movq  %rbx, %xmm2
+  movddup %xmm0, %xmm0
+  movddup %xmm2, %xmm2
   
   pclmulqdq $18, %xmm1, %xmm2
   
   movq  $0xFEFEFEFEFEFEFEFE,  %rax
+  movq  $0,  %rbx
   movq  %rax, %xmm0
-  movddup %xmm0, %xmm1
+  movq  %rbx, %xmm2
+  movddup %xmm0, %xmm0
+  movddup %xmm2, %xmm2
   
   pclmulqdq $255, %xmm1, %xmm2
 
