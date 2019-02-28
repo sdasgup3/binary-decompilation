33ea702083ad2c9a3a10c404a4406e57fd3a55d2
Sandeep Dasgupta <sdasgup3@illinois.edu>
2019-02-27 21:48:26 -0600

    sail support

--- x86-semantics/docs/relatedwork/sail/run.sh
+++ x86-semantics/docs/relatedwork/sail/run.sh
@@ -1,54 +1,59 @@
 #!/bin/bash
-set -ex
+#set -ex
 
 IDEAL=~/Github/binary-decompilation/x86-semantics/docs/relatedwork/k-semantics/current_support.txt
+OUT=~/Github/binary-decompilation/x86-semantics/docs/relatedwork/sail/support.txt
 
-grep "^add._" $IDEAL
-grep "^or._" $IDEAL
-grep "^adc._" $IDEAL
-grep "^sbb._" $IDEAL
-grep "^and._" $IDEAL
-grep "^sub._" $IDEAL
-grep "^xor._" $IDEAL
-grep "^cmp._" $IDEAL
-grep "^rol._" $IDEAL
-grep "^ror._" $IDEAL
-grep "^rcl._" $IDEAL
-grep "^rcr._" $IDEAL
-grep "^shl._" $IDEAL
-grep "^shr._" $IDEAL
-grep "^test._" $IDEAL
-grep "^sar._" $IDEAL
-grep "^dec._" $IDEAL
-grep "^inc._" $IDEAL
-grep "^neg._" $IDEAL
-grep "^not._" $IDEAL
-grep "^sar._" $IDEAL
-grep "^bts._" $IDEAL
-grep "^btc._" $IDEAL
-grep "^btr._" $IDEAL
-grep "^push._" $IDEAL
-grep "^pop._" $IDEAL
-grep "^call._" $IDEAL
-grep "^clc" $IDEAL
-grep "^cmc" $IDEAL
-grep "^cmpxchg._" $IDEAL
-grep "^div._" $IDEAL
-echo "hlt"
-echo "lfence"
-echo "mfence"
-echo "fence"
-echo "hlt"
-grep "^j" $IDEAL
-grep "^lea._" $IDEAL
-grep "^leave._" $IDEAL
-grep "^loop" $IDEAL
-grep "^mov._" $IDEAL
-grep "^movs[bwlq][bwlq]_" $IDEAL
-grep "^mul[bwlq]_" $IDEAL
-grep "^nop" $IDEAL
-grep "^ret" $IDEAL
-grep "^set" $IDEAL
-grep "^stc" $IDEAL
-grep "^xadd[bwlq]_" $IDEAL
-grep "^xchg[bwlq]_" $IDEAL
+rm -rf $OUT
+grep "^add[blwq]_" $IDEAL  > $OUT
+grep "^or[blwq]_" $IDEAL >> $OUT
+grep "^adc[blwq]_" $IDEAL >> $OUT
+grep "^sbb[blwq]_" $IDEAL >> $OUT
+grep "^and[blwq]_" $IDEAL >> $OUT
+grep "^sub[blwq]_" $IDEAL >> $OUT
+grep "^xor[blwq]_" $IDEAL >> $OUT
+grep "^cmp[blwqs]_" $IDEAL >> $OUT
+grep "^rol[blwq]_" $IDEAL >> $OUT
+grep "^ror[blwq]_" $IDEAL >> $OUT
+grep "^rcl[blwq]_" $IDEAL >> $OUT
+grep "^rcr[blwq]_" $IDEAL >> $OUT
+grep "^shl[blwq]_" $IDEAL >> $OUT
+grep "^shr[blwq]_" $IDEAL >> $OUT
+grep "^test[blwq]_" $IDEAL >> $OUT
+grep "^sar[blwq]_" $IDEAL >> $OUT
+grep "^dec[blwq]_" $IDEAL >> $OUT
+grep "^inc[blwq]_" $IDEAL >> $OUT
+grep "^neg[blwq]_" $IDEAL >> $OUT
+grep "^not[blwq]_" $IDEAL >> $OUT
+grep "^sar[blwq]_" $IDEAL >> $OUT
+grep "^bts[blwq]_" $IDEAL >> $OUT
+grep "^btc[blwq]_" $IDEAL >> $OUT
+grep "^btr[blwq]_" $IDEAL >> $OUT
+grep "^push[blwq]_" $IDEAL >> $OUT
+grep "^pop[blwq]_" $IDEAL >> $OUT
+grep "^call[blwq]_" $IDEAL >> $OUT
+grep "^clc" $IDEAL >> $OUT
+grep "^cmc" $IDEAL >> $OUT
+grep "^cmpxchg[blwq]_" $IDEAL >> $OUT
+grep "^div[blwq]_" $IDEAL >> $OUT
+echo "hlt" >> $OUT
+echo "lfence" >> $OUT
+echo "mfence" >> $OUT
+echo "fence" >> $OUT
+grep "^j" $IDEAL >> $OUT
+grep "^lea[blwq]_" $IDEAL >> $OUT
+grep "^leave[blwq]_" $IDEAL >> $OUT
+grep "^loop" $IDEAL >> $OUT
+grep "^mov[blwq]_" $IDEAL >> $OUT
+grep "^movs[bwlq][bwlq]_" $IDEAL >> $OUT
+grep "^mul[bwlq]_" $IDEAL >> $OUT
+grep "^nop" $IDEAL >> $OUT
+grep "^ret" $IDEAL >> $OUT
+grep "^set" $IDEAL >> $OUT
+grep "^stc" $IDEAL >> $OUT
+grep "^xadd[bwlq]_" $IDEAL >> $OUT
+grep "^xchg[bwlq]_" $IDEAL >> $OUT
+
+sort $OUT | uniq > /tmp/x
+mv /tmp/x $OUT
+wc $OUT
