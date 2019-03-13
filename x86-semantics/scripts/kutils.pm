19a795bf3dd5d01de12b2085edbadd6d538fc0f4
Sandeep Dasgupta <sdasgup3@illinois.edu>
2019-03-12 19:07:54 -0500

    minor scripts update

--- x86-semantics/scripts/kutils.pm
+++ x86-semantics/scripts/kutils.pm
@@ -20,11 +20,11 @@ $VERSION = 1.00;
   qw(processKFile checkKRunStatus processXFile compareStates pprint find_stratum getReadMod spec_template getSpecCode selectbraces mixfix2infix processSpecOutput sanitizeSpecOutput writeKDefn opcHasOperand instrGetOperands runkprove postProcess createSpecFile checkSupported checkManuallyGenerated getImmInstrs getMemInstrs generateZ3Formula modelInstructions assocateMcSemaXed assocateMcSemaAvail assocIntelATT getTargetInstr getStrataBVFormula getRegVaraint  mem_modify_testcases getInstrsFolder getDummyRegsForOperands getOperandListFromOpcode getOperandListFromInstr sanitizeBVF parseKFile);
 @EXPORT_OK = qw();
 
-my $home = $ENV{"HOME"};
+my $home = "";
 
 BEGIN {
-    $home = $ENV{"HOME"};
-    unshift @INC, "$home/Github/binary-decompilation/x86-semantics/scripts/";
+    my $script_dir = dirname(__FILE__);
+    unshift @INC, $script_dir;
 }
 use utils;
 
@@ -5137,7 +5137,7 @@ sub assocIntelATT {
     my $verifyCount = 0;
 
     ## Model instruction in a map.
-    open( my $fp, "<", $filename ) or die "Can't open $filename: $!";
+    open( my $fp, "<", $filename ) or die "Can't open: $!";
     my @lines = <$fp>;
     close $fp;
 
